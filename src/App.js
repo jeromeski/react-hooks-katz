464001d5cfa6b5d59a2c7772f787152960a7e0ba
jeromeski <jeromegee@gmail.com>
2021-03-14 08:22:30 +0800

    First hook useState()

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => {
-	return <div></div>;
+function App() {
+  const [userQuery, setUserQuery] = useState('')
+
+  const updateUserQuery = (e) => {
+    console.log(e.target.value)
+    setUserQuery(e.target.value)
+  };
+
+	const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`)
+  };
+
+  const handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      searchQuery()
+    }
+  }
+
+	return <div className='App'>
+    <h1>Hello David</h1>
+    <div className="form">
+      <input type="text" value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress}/>
+      <button onClick={searchQuery}>Search</button>
+    </div>
+  </div>;
 };
 
 export default App;
